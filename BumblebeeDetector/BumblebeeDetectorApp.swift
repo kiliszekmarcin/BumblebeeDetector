51f18570c8b6c99e583ca5efae74a94ca2858acf
Marcin Kiliszek <kiliszekmarcin@gmail.com>
2021-01-05 20:07:31 +0100

    Initial Commit

---
+++ BumblebeeDetector/BumblebeeDetectorApp.swift
@@ -0,0 +1,17 @@
+//
+//  BumblebeeDetectorApp.swift
+//  BumblebeeDetector
+//
+//  Created by Marcin Kiliszek on 05/01/2021.
+//
+
+import SwiftUI
+
+@main
+struct BumblebeeDetectorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
