3d8c2d7b15933f9406a4b9abd756798ca690573c
Marcin Kiliszek <mkiliszek1@sheffield.ac.uk>
2021-03-02 20:27:08 +0000

    add functions to fetch date and location from a video

--- BumblebeeDetector/Util/Utils.swift
+++ BumblebeeDetector/Util/Utils.swift
@@ -8,8 +8,40 @@
 import Foundation
 import UIKit
 import AVFoundation
+import CoreLocation
 
 class Utils {
+    static func getVideoMetadata(url: URL) -> (firstFrame: UIImage?, location: CLLocation?, date: Date?) {
+        var firstFrame: UIImage? = nil
+        var location: CLLocation? = nil
+        var date: Date? = nil
+        
+        let asset = AVAsset(url: url)
+        
+        // get date
+        date = asset.creationDate?.dateValue
+        
+        // get location
+        let metadata = asset.commonMetadata
+        let locationIdentifier = AVMetadataIdentifier.commonIdentifierLocation
+        if let locationString = AVMetadataItem.metadataItems(from: metadata, filteredByIdentifier: locationIdentifier).first?.stringValue {
+            location = iso6709ToCLLocation(locationString: locationString)
+        }
+        
+        // get first frame
+        let generator = AVAssetImageGenerator(asset: asset)
+        
+        do {
+            firstFrame = UIImage(cgImage: try generator.copyCGImage(at: CMTime(seconds: 0.0, preferredTimescale: 600), actualTime: nil))
+        } catch let error {
+            print("Error when extracting first frame after video selection")
+            print(error.localizedDescription)
+        }
+        
+        // return
+        return (firstFrame, location, date)
+    }
+    
     static func getVideoFirstFrame(url: URL) -> UIImage? {
         let asset = AVAsset(url: url)
         let generator = AVAssetImageGenerator(asset: asset)
@@ -19,6 +51,20 @@ class Utils {
         } catch let error {
             print("Error when extracting first frame after video selection")
             print(error.localizedDescription)
+        }
+        
+        return nil
+    }
+    
+    static func iso6709ToCLLocation(locationString: String) -> CLLocation? {
+        let indexLat = locationString.index(locationString.startIndex, offsetBy: 8)
+        let indexLong = locationString.index(indexLat, offsetBy: 9)
+
+        let lat = String(locationString[locationString.startIndex..<indexLat])
+        let long = String(locationString[indexLat..<indexLong])
+
+        if let lattitude = Double(lat), let longitude = Double(long) {
+            return CLLocation(latitude: lattitude, longitude: longitude)
         }
         
         return nil
